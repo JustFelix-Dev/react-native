b20c3fa4a4a04ea3c3abd2eb9f28ae448bc6cd17
Felix <owolabifelix78@gmail.com>
2024-09-13 17:17:07 +0100

    feat: switch component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { ActivityIndicator, Alert, Button, Dimensions, FlatList, Image, ImageBackground, Modal, Platform, Pressable, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import { ActivityIndicator, Alert, Button, Dimensions, FlatList, Image, ImageBackground, Modal, Platform, Pressable, SafeAreaView, ScrollView, StatusBar, StyleSheet, Switch, Text, TextInput, View, useWindowDimensions } from 'react-native';
 import Greet from './components/Greet';
 import Box from './components/Box';
 import CustomButton from './components/CustomButtons/CustomButton';
@@ -9,6 +9,11 @@ import ListItem from "./data.json";
 const App = () => {
 
   const [ isModalVisible,setIsModalVisible] = useState(false);
+  const [ name,setName ] = useState();
+  const [ isDarkMode,setIsDarkMode ]= useState(false);
+  const handleMode = ()=>{
+    setIsDarkMode((prev)=> !prev)
+  }
   // const [ dimensions,setDimensions] = useState({
   //   window: Dimensions.get('window')
   // })
@@ -62,6 +67,18 @@ const App = () => {
         }
        }),
        fontSize: windowWidth > 500 ? 50 : 24,
+    },
+    input:{
+      height:40,
+      margin:10,
+      padding:10,
+      borderWidth:2,
+      borderRadius: 10
+
+    },
+    multiline:{
+      minHeight:100,
+      textAlignVertical:'top'
     }
   })
 
@@ -105,7 +122,7 @@ const App = () => {
      </Pressable>
 
      <Pressable onPress={()=> console.log('Text Pressed!')}>
-     <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, ut doloribus assumenda doloremque nostrum amet quo rem! Aliquid veniam quam fugit eius, nemo sapiente? Temporibus totam numquam incidunt at ducimus doloremque voluptate nemo quos blanditiis, rem tempore aspernatur minus optio eos similique sapiente, repellendus laudantium voluptates nesciunt consequuntur esse minima. Impedit quia praesentium optio officiis eius nihil totam commodi, molestiae, sunt veniam neque. Voluptatibus, maxime unde aliquid omnis expedita voluptas deserunt! Repellat repellendus sunt cum nesciunt necessitatibus amet voluptas? Repudiandae, in? Laudantium ut dicta aliquam suscipit velit alias debitis fugit ratione illo possimus beatae ad cupiditate eius consequuntur distinctio facere cum tenetur pariatur, reprehenderit iste totam veritatis atque voluptates minima. Illo itaque suscipit praesentium ipsam doloremque consectetur velit a, architecto asperiores accusantium dolorum rerum similique repellat ea. Omnis deleniti repellendus dolorem dolor aliquid cum quod necessitatibus. Ex animi tempora ducimus non natus voluptates, porro totam nemo itaque, illum odit ad officia delectus maxime hic facilis quos eos sapiente corrupti aperiam. Neque suscipit voluptatem necessitatibus nam autem voluptates quam illum? Doloribus neque numquam aliquam maiores architecto aperiam, vel veritatis facilis quidem ut! Consequatur ullam, architecto placeat quibusdam harum facere eveniet, vel, nobis voluptatem aut laborum. Temporibus ratione quae iure eius repellendus earum illo aliquid aperiam dolorum! Aliquam nam ratione praesentium assumenda labore, vel ipsum et facilis reprehenderit, pariatur tempora, fugiat neque voluptatem. Perspiciatis assumenda, minus, fugiat eius reprehenderit totam aspernatur, nulla enim rem cumque blanditiis! Explicabo possimus commodi asperiores? Enim, tenetur. Blanditiis sed temporibus quam eius alias non aperiam praesentium tenetur iste officia earum obcaecati molestiae, dolorem unde sapiente. Veniam animi unde accusamus ratione numquam repellendus eius in itaque blanditiis voluptatum consectetur perferendis ut dolorem modi cupiditate nihil inventore nulla maiores, placeat asperiores eveniet. Vero architecto iure nisi voluptas atque quas molestiae consectetur non similique! Explicabo cum repellendus architecto similique ducimus.</Text>
+     <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat, ut doloribus assumenda doloremque nostrum amet quo rem! Aliquid veniam quam fugit eius, nemo sapiente? Temporibus totam numquam incidunt at ducimus doloremque voluptate nemo quos blanditiis, rem tempore aspernatur minus optio eos similique sapiente, repellendus laudantium voluptates nesciunt consequuntur esse minima. Impedit quia praesentium optio officiis eius nihil totam commodi, molestiae, sunt veniam neque. Voluptatibus, maxime unde aliquid omnis expedita voluptas deserunt! Repellat repellendus sunt cum nesciunt necessitatibus amet voluptas? Repudiandae, in? Laudantium ut dicta aliquam suscipit velit alias debitis fugit ratione illo possimus beatae ad cupiditate eius consequuntur distinctio facere cum tenetur pariatur, reprehenderit iste totam veritatis atque voluptates minima. Illo itaque suscipit praesentium ipsam doloremque consectetur velit a, architecto asperiores accusantium dolorum rerum similique repellat ea. Omnis deleniti repellendus dolorem dolor aliquid cum quod necessitatibus. Ex animi tempora ducimus non natus voluptates, porro totam nemo itaque, illum odit ad officia delectus maxime hic facilis quos eos sapiente corrupti aperiam. Neque suscipit voluptatem necessitatibus nam autem voluptates quam illum? Doloribus neque numquam aliquam maiores architecto aperiam, vel veritatis facilis quidem ut! Consequatur ullam, architecto placeat quibusdam harum facere eveniet, vel, nobis voluptatem aut laborum. Temporibus ratione quae iure eius repellendus earum illo aliquid aperiam dolorum!</Text>
      </Pressable>
      <Image source={logoImg} style={{height:150,width:150}}/>
     {/* <Text>Hello World</Text>
@@ -156,7 +173,22 @@ const App = () => {
       keyExtractor={(item,index)=> item.id.toString()}
       ItemSeparatorComponent={()=><View style={{height:15}}></View>}
       ListEmptyComponent={<Text>No Item Found!</Text>}
+      ListHeaderComponent={<Text style={{fontSize:24}}>Admin List:</Text>}
+      ListFooterComponent={<Text style={{fontSize:24}}>End of the  List</Text>}
       />
+
+      <View>
+        <Text>Forms:</Text>
+        <TextInput  style={styles.input} placeholder='email@example.com'  secureTextEntry keyboardType='default' value={name} onChangeText={setName} />
+        <Text style={{fontSize:20}}>My name is {name}</Text>
+        <TextInput style={[styles.input,styles.multiline]} placeholder='Enter your message ...' multiline/>
+        <View style={{padding:10,height:200}}>
+        <Text>Dark Mode:</Text>
+        <Switch value={isDarkMode} onValueChange={handleMode} trackColor={{ false:"#767577",true:"green"}}
+        thumbColor={{false:"grey"}}
+         />
+        </View>
+      </View>
      </View>
     </ScrollView>
    </View>
