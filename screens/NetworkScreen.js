897a40bcb3ffa71140443a000e929614de37e6f8
Felix <owolabifelix78@gmail.com>
2024-09-17 21:05:58 +0100

    feat: pull refreshing

--- screens/NetworkScreen.js
+++ screens/NetworkScreen.js
@@ -4,6 +4,7 @@ import { ActivityIndicator, FlatList, StatusBar, StyleSheet, Text, View } from '
 const NetworkScreen = () => {
   const [postList, setPostList] = useState([]);
   const [ isLoading,setIsLoading] = useState(true);
+  const [refreshing, setRefreshing ] = useState(false);
 
     const fetchData = async( limit=10 )=>{
         const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`)
@@ -17,6 +18,12 @@ const NetworkScreen = () => {
     useEffect(()=>{
        fetchData()
     },[])
+
+    const handleRefresh = ()=>{
+        setRefreshing(true)
+        fetchData(20)
+        setRefreshing(false)
+    }
 
 const styles = StyleSheet.create({
    listContainer:{
@@ -60,6 +67,8 @@ const styles = StyleSheet.create({
         ListEmptyComponent={<Text>No List Found!</Text>}
         ListHeaderComponent={<Text>List of Posts:</Text>}
         ListFooterComponent={<Text>Post ended here!</Text>}
+        refreshing={refreshing}
+        onRefresh={handleRefresh}
         />
      </View>
   )
