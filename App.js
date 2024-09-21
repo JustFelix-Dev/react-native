689378330dd09ff4699c889ae80744e8d5e52a15
Felix <owolabifelix78@gmail.com>
2024-09-21 07:11:16 +0100

    feat: route parameters

--- App.js
+++ App.js
@@ -14,12 +14,30 @@ import NetworkScreen from './screens/NetworkScreen';
 const Stack = createNativeStackNavigator();
 const App = () => {
   
+  // const screenOptions = {
+  //    title:"Home Screen",
+  //    head
+  // }
   return (
-
     <NavigationContainer>
-      <Stack.Navigator  initialRouteName='Home'>
+      <Stack.Navigator  initialRouteName='Home' 
+      screenOptions={{
+       headerStyle:{
+        backgroundColor: "#6a51ae",
+       },
+       headerTintColor:"#fff",
+       headerTitleStyle: { fontWeight:"bold"},
+       headerRight: ()=>(
+        <Pressable onPress={()=> alert('Menu button pressed!')}>
+          <Text style={{ color:"fff",fontSize:16 }} >Menu</Text>
+        </Pressable>
+       ),
+       contentStyle: {
+         backgroundColor:"#333"
+       },
+      }}>
         <Stack.Screen name='Home' component={HomeScreen}/>
-        <Stack.Screen name='About' component={AboutScreen}/>
+        <Stack.Screen name='About' component={AboutScreen} initialParams={{name:"Guest"}}/>
         <Stack.Screen name='Network' component={NetworkScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Button, Text, View } from 'react-native'
 
-const AboutScreen = ({navigation}) => {
+const AboutScreen = ({navigation,route}) => {
+  const { name } = route.params;
   return (
    <View>
-    <Text>Welcome to the About Screen!</Text>
+    <Text>Welcome to the About Screen {name}!</Text>
     <Button title='Go to Network Screen' onPress={()=> navigation.navigate('Network')}/>
    </View>
   )
